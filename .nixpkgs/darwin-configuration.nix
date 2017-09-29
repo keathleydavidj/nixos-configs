2988cee443179e3615bd7f5e1f3e17f3b602636a
David Keathley <dkeathley@dkeathley-mbp.local>
2017-09-28 22:39:21 -0500

    add nix-darwin configuration.nix

---
+++ .nixpkgs/darwin-configuration.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+{
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [ pkgs.nix-repl
+    ];
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.bash.enable = true;
+
+  # Recreate /run/current-system symlink after boot.
+  services.activate-system.enable = true;
+
+  # You should generally set this to the total number of logical cores in your system.
+  # $ sysctl -n hw.ncpu
+  nix.maxJobs = 4;
+}
